b5a6c0982ec004f4347efb0ac73d7698ed48a92b
Mikkel Simon Ølting Jensen <msoj71393@edu.ucl.dk>
2026-03-18 10:55:38 +0100

    Made some changes to the layout of the main page and removed some pages we didn't use. Also I made changes to where some functions were and how they're called.

---
+++ BlazorApp2test/BlazorApp2test/Models/ResidentHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp2test.Models
+{
+    public static class ResidentHelper
+    {
+        public static string GetRisikoClass(Risiko risiko)
+        {
+            if (risiko == Risiko.Groen)
+                return "risiko-groen";
+            else if (risiko == Risiko.Gul)
+                return "risiko-gul";
+            else if (risiko == Risiko.Roed)
+                return "risiko-roed";
+            else
+                return "";
+        }
+    }
+}
